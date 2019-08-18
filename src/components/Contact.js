fabd77a6c182f2167f8fd9710fa3cedbb979a50e
Manuel Valles <manuel.valles@everis.com>
2019-08-18 13:50:10 +0100

    Added the Context Reducer for actions to Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Consumer } from '../context';
 
 class Contact extends Component {
   state = {
@@ -11,42 +12,51 @@ class Contact extends Component {
     }));
   };
 
-  onDeleteClick = () => {
-    this.props.deleteClickHandler();
+  onDeleteClick = (id, dispatch) => {
+    dispatch({
+      type: 'DELETE_CONTACT',
+      payload: id
+    });
   };
 
   render() {
-    const { name, email, phone } = this.props.contact;
+    const { name, email, phone, id } = this.props.contact;
     const { showContactInfo } = this.state;
     return (
-      <div className="card card-body mb-3">
-        <h4>
-          {name}{' '}
-          <i
-            onClick={this.onToggle}
-            className="fas fa-sort-down"
-            style={{ cursor: 'pointer' }}
-          />
-          <i
-            onClick={this.onDeleteClick}
-            className="far fa-trash-alt"
-            style={{ cursor: ' pointer', float: 'right', color: 'red' }}
-          />
-        </h4>
-        {showContactInfo && (
-          <ul className="list-group">
-            <li className="list-group-item">Email: {email}</li>
-            <li className="list-group-item">Phone: {phone}</li>
-          </ul>
-        )}
-      </div>
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className="card card-body mb-3">
+              <h4>
+                {name}{' '}
+                <i
+                  onClick={this.onToggle}
+                  className="fas fa-sort-down"
+                  style={{ cursor: 'pointer' }}
+                />
+                <i
+                  onClick={this.onDeleteClick.bind(this, id, dispatch)}
+                  className="far fa-trash-alt"
+                  style={{ cursor: ' pointer', float: 'right', color: 'red' }}
+                />
+              </h4>
+              {showContactInfo && (
+                <ul className="list-group">
+                  <li className="list-group-item">Email: {email}</li>
+                  <li className="list-group-item">Phone: {phone}</li>
+                </ul>
+              )}
+            </div>
+          );
+        }}
+      </Consumer>
     );
   }
 }
 
 Contact.propTypes = {
-  contact: PropTypes.object.isRequired,
-  deleteClickHandler: PropTypes.func.isRequired
+  contact: PropTypes.object.isRequired
 };
 
 export default Contact;
